ca284449cb07225e5b8552eb515962a4898c54d2
susankelvin <susankelvin@users.noreply.github.com>
2014-12-22 01:42:17 +0200

    Passport module and its configuration added.

---
+++ configuration/passport.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var userManager = require('../data/userManager');
+
+function config(app) {
+    passport.use(new LocalStrategy(
+        function (username, password, done) {
+            userManager.login(username, password, function (err, user) {
+                if (err) {
+                    return done(err);
+                }
+
+                if (!user) {
+                    return done(null, false);
+                }
+
+                return done(null, user);
+            });
+        }
+    ));
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        userManager.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+    app.use(passport.initialize());
+    app.use(passport.session());
+}
+
+module.exports = {
+    config: config
+};
