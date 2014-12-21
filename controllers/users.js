e66f17405822730fea00acb156d25d387f63ed7a
susankelvin <susankelvin@users.noreply.github.com>
2014-12-22 01:43:08 +0200

    User registration view and basic logic.

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,33 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
+var userManager = require('../data/userManager');
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+//router.get('/', function (req, res) {
+//    res.send('respond with a resource');
+//});
+
+router.get('/register', function (req, res) {
+    res.render('users/register');
+});
+
+router.post('/register', function (req, res) {
+    var username = req.body.username,
+        password = req.body.password;
+
+    userManager.register(username, password, function(err, user){
+        if (err) {
+            res.redirect('/users/register');
+        }
+        else if (user) {
+            res.redirect('/');
+        }
+        else {
+            res.redirect('/users/register');
+        }
+    });
 });
 
 module.exports = router;
--- data/userManager.js
+++ data/userManager.js
@@ -4,17 +4,14 @@ var SALT_SIZE = 256;
 var crypto = require('crypto');
 var User = require('../models/User');
 
+// callback(err, user)
 function register(username, password, callback) {
     var salt,
-        passwordHash,
-        hmac,
-        user;
+        user,
+        passwordHash;
 
     salt = crypto.randomBytes(SALT_SIZE).toString('hex');
-    hmac = crypto.createHmac('md5', salt);
-    hmac.update(password);
-    passwordHash = hmac.digest('hex');
-
+    passwordHash = hashPassword(password, salt);
     user = new User({
         username: username,
         passwordHash: passwordHash,
@@ -23,6 +20,48 @@ function register(username, password, callback) {
     user.save(callback);
 }
 
+// callback(err, user)
+function login(username, password, callback){
+    var user,
+        passwordHash;
+
+        user = User.find({username: username}, function(err, user) {
+            if (err) {
+                callback(err);
+            }
+            else if (user) {
+                passwordHash = hashPassword(password, user.salt);
+                if (passwordHash === user.passwordHash) {
+                    callback(null, user);
+                }
+                else {
+                    callback(null, false);
+                }
+            }
+            else {
+                callback(null, false);
+            }
+        });
+}
+
+// callback(err, user)
+function findById(id, callback){
+    var user = User.findById(id, callback);
+}
+
+// Returns password hmac
+function hashPassword(password, salt) {
+    var passwordHash,
+        hmac;
+
+    hmac = crypto.createHmac('md5', salt);
+    hmac.update(password);
+    passwordHash = hmac.digest('hex');
+    return passwordHash;
+}
+
 module.exports = {
-    register: register
+    register: register,
+    login: login,
+    findById: findById
 };
