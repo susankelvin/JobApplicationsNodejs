71b60a0d116d02182d249d2a0b10460cfd244801
susankelvin <susankelvin@users.noreply.github.com>
2014-12-21 14:19:33 +0200

    User schema and model.

---
+++ models/User.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema();
+
+var userSchema = new Schema({
+        username: {type: String, required: true},
+        passwordHash: {type: String, required: true},
+        salt: {type: String, required: true},
+        registeredOn: {type: Date, default: new Date()}
+    }
+);
+
+var User = mongoose.model('User', userSchema);
+module.exports = User;
